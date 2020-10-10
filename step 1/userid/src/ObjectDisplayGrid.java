66620a2273f806badef52800debee48ec35ee113
isabelleakian <isabelle.akian@gmail.com>
2020-10-10 16:43:20 -0400

    adding bottomHeight to objectDisplayGrid

--- step 1/userid/src/ObjectDisplayGrid.java
+++ step 1/userid/src/ObjectDisplayGrid.java
@@ -1,6 +1,6 @@
 public class ObjectDisplayGrid{
 
-    protected int gameHeight, width, topHeight;
+    protected int gameHeight, width, topHeight, bottomHeight;
 
     public void getObjectDisplayGrid(int gameHeight, int width, int topHeight){
         System.out.println("ObjectDisplayGrid: getObjectDisplayGrid: gameHeight: " + gameHeight + ", width: " + width + ", topHeight: " + topHeight);
